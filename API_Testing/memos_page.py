058accce5a4b52ee78114147143967a30cc2697d
Dong Yuanyuan <dawnyuan28@gmail.com>
2026-04-13 10:17:28 +0800

    feat: 分支修改测试1017

--- API_Testing/memos_page.py
+++ API_Testing/memos_page.py
@@ -38,5 +38,7 @@ class MemosPage:
 
     def memo_exists(self, memo_content: str) -> bool:
         return self.get_memo_card(memo_content).count() > 0
-#测试是否合并分支git branchgit log --oneline
+
+
+#测试是否合并分支git branchgit log --oneline 1016
 print("test branch change")
